73c40974b194d5c26245b66dd0df198c5634efa7
Artitaya50217 <artitaya2466@gmail.com>
2021-01-30 19:50:07 +0700

    add score

---
+++ assignments/Exercise_Artitaya_Y.py
@@ -0,0 +1,9 @@
+Foundation = 98
+General = 80
+Introduction = 85
+Computer = 90
+
+print("Foundation English : ",Foundation)
+print("General Business : ",General)
+print("Introduction to Computer Systems : ",Introduction)
+print("Computer Programming : ",Computer)
